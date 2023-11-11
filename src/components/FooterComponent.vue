e455ef8b3b793acd3f6c6af293e6f57ce317260e
Artem Kriuchkov <tomganchos@mail.ru>
2023-11-11 23:26:58 +0200

    update footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    Footer
+    <span>© </span><strong>Hind</strong>ikutsikas, 2023
   </footer>
 </template>
 
@@ -11,10 +11,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 footer {
   height: 72px;
   width: 100%;
   background-color: #ccc;
+  padding: 16px;
+  display: flex;
+  align-items: center;
+
+  & span {
+    margin-right: 4px;
+  }
 }
 </style>
